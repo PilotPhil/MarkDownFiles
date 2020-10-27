26f2944d60a7a14074fc365d93fa5fe6c23ba401
17788360594 <632913013@qq.com>
2020-10-27 23:23:05 +0800

    add

---
+++ temp/ray1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ray1 : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    // 创建射线到屏幕上的参考点，像素坐标  
+    Vector3 position = new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0.0f);
+
+    void Update()
+    {
+        // 射线沿着屏幕x轴从左向右循环扫描  
+        position.x = position.x >= Screen.width ? 0.0f : position.x + 1.0f;
+        // 生成射线  
+        ray = Camera.main.ScreenPointToRay(position);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            // 如果与物体发生碰撞，在Scene视图中绘制射线  
+            Debug.DrawLine(ray.origin, hit.point, Color.green);
+            // 打印射线检测到的物体的名称  
+            Debug.Log("射线检测到的物体名称: " + hit.transform.name);
+        }
+    }
+}
---
+++ temp/ray2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ray2 : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    // 创建射线到屏幕上的参考点，单位化坐标  
+    Vector3 position = new Vector3(0.5f, 0.5f, 0.0f);
+    void Update()
+    {
+        // 射线沿着屏幕x轴从左向右循环扫描  
+        position.x = position.x >= 1.0f ? 0.0f : position.x + 0.02f;
+        // 生成射线  
+        ray = Camera.main.ViewportPointToRay(position);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            // 如果与物体发生碰撞，在Scene视图中绘制射线  
+            Debug.DrawLine(ray.origin, hit.point, Color.green);
+            // 打印射线检测到的物体的名称  
+            Debug.Log("射线检测到的物体名称: " + hit.transform.name);
+        }
+    }
+}
